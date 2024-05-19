b2b3592b175fa79889b30b3cf42fa59f92658f51
MaximeTTS <maxime.s.turquet@gmail.com>
2024-05-19 18:03:13 +0200

    Changed the order in the stuff.js route code, which corrected the no-display of top-rated books.

--- backend/rootes/stuff.js
+++ backend/rootes/stuff.js
@@ -5,11 +5,11 @@ const bookController = require("../controllers/stuff");
 const multer = require("../middelware/multer-config");
 
 router.get("/", auth, bookController.getAllBooks);
-router.post("/", auth, multer, bookController.createBook);
+router.get('/bestrating', bookController.getBestRating);
 router.get("/:id", auth, bookController.getOneBook);
+router.post('/:id/rating', auth, bookController.rateBook);
+router.post("/", auth, multer, bookController.createBook);
 router.put("/:id", auth, multer, bookController.modifyBook);
 router.delete("/:id", auth, bookController.deleteBook);
-router.post('/:id/rating', auth, bookController.rateBook);
-router.get('/bestrating', bookController.getBestRating);
 
 module.exports = router;
