d340ca8620c780fdc0144a55b400734ddc72c4f2
MaximeTTS <maxime.s.turquet@gmail.com>
2024-05-21 02:00:53 +0200

    Changed the modifyBook code in stuff.js to delete the previous image if the user changes on oder images.

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -32,15 +32,26 @@ exports.modifyBook = (req, res, next) => {
     : { ...req.body };
 
   delete bookObject._userId;
+
   Book.findOne({ _id: req.params.id })
     .then((book) => {
       if (book.userId != req.auth.userId) {
-        res.status(401).json({ message: "Non autorisé" });
-      } else {
-        Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
-          .then(() => res.status(200).json({ message: "Livre modifié !" }))
-          .catch((error) => res.status(401).json({ error }));
+        return res.status(401).json({ message: "Non autorisé" });
       }
+
+      const oldImageUrl = book.imageUrl;
+
+      Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+        .then(() => {
+          if (req.file) {
+            const oldFilename = oldImageUrl.split('/images/')[1];
+            fs.unlink(`images/${oldFilename}`, (err) => {
+              if (err) console.log(err);
+            });
+          }
+          res.status(200).json({ message: "Livre modifié !" });
+        })
+        .catch((error) => res.status(401).json({ error }));
     })
     .catch((error) => res.status(400).json({ error }));
 };
