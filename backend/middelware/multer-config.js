ebaa446241b1a705d6d15500432a726663f0aaac
MaximeTTS <maxime.s.turquet@gmail.com>
2024-05-21 02:27:54 +0200

    Optimize image size and weight by adding sharp to the multer-config.js code

--- backend/middelware/multer-config.js
+++ backend/middelware/multer-config.js
@@ -1,4 +1,6 @@
 const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
 
 const MIME_TYPES = {
   "image/jpg": "jpg",
@@ -6,15 +8,29 @@ const MIME_TYPES = {
   "image/png": "png",
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
-  },
-});
+const storage = multer.memoryStorage();
 
-module.exports = multer({ storage: storage }).single("image");
+const upload = multer({ storage: storage }).single("image");
+
+const processImage = (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  const extension = MIME_TYPES[req.file.mimetype];
+  const filename = req.file.originalname.split(" ").join("_") + Date.now() + "." + extension;
+  const filepath = path.join("images", filename);
+
+  sharp(req.file.buffer)
+    .resize(800)
+    .toFormat(extension, { quality: 80 })
+    .toFile(filepath, (err, info) => {
+      if (err) {
+        return next(err);
+      }
+      req.file.filename = filename;
+      next();
+    });
+};
+
+module.exports = { upload, processImage };
--- backend/rootes/stuff.js
+++ backend/rootes/stuff.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middelware/auth");
 const bookController = require("../controllers/stuff");
-const multer = require("../middelware/multer-config");
+const { upload, processImage } = require("../middelware/multer-config");
 
 router.get("/", bookController.getAllBooks);
 router.get('/bestrating', bookController.getBestRating);
 router.get("/:id", bookController.getOneBook);
 router.post('/:id/rating', auth, bookController.rateBook);
-router.post("/", auth, multer, bookController.createBook);
-router.put("/:id", auth, multer, bookController.modifyBook);
+router.post("/", auth, upload, processImage, bookController.createBook);
+router.put("/:id", auth, upload, processImage, bookController.modifyBook);
 router.delete("/:id", auth, bookController.deleteBook);
 
 module.exports = router;
